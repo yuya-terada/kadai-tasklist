ebbad4481d9f1df0e29d2d8cf199ae766c26340d
jiro.kirameki <ec2-user@ip-172-31-41-166.us-east-2.compute.internal>
2018-05-30 01:29:20 +0000

    Procfile

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,7 +5,7 @@
 <h1>task新規作成ページ</h1>
 
     <div class="row">
-        <div class="col-xs-12","col-sm-offset-2 col-sm-8","col-md-offset-2 col-md-8", "col-lg-offset-3 col-lg-6">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
             
