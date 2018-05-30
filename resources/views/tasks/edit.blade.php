c9927b80a0eda05c1356516b80cc7795ff72e755
jiro.kirameki <ec2-user@ip-172-31-41-166.us-east-2.compute.internal>
2018-05-30 06:22:06 +0000

    comment

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -8,8 +8,8 @@
         <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
             {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
                 <div class="form-group">
-                    {!! Form::label('title', 'status:') !!}
-                    {!! Form::text('title', null, ['class' => 'form-control']) !!}
+                    {!! Form::label('status', 'status:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
                 </div>
         
                 <div class="form-group">
