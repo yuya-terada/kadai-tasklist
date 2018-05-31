21f3a116e5caba1fa48095b9c07ad4ce62196b65
jiro.kirameki <ec2-user@ip-172-31-41-166.us-east-2.compute.internal>
2018-05-31 06:17:46 +0000

    comment

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<h1>id: {{ $task->id }} のtask編集ページ</h1>
+<h1>id: {{ $task->id }} のtask編?集ページ</h1>
 
    <div class="row">
         <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
